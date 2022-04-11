4a9cd1c0deb514ea495c270c7a19eb1aee2aa570
Cindy <cindyho5241@gmail.com>
2022-04-11 05:13:05 -0700

    added more function values

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 //This is lab2
 /* Winning vals: 123,456, 789, 147, 258, 369, 159, 357 */
 
-function reset();
-function checkBoard();
-function switchTurns();
 
+function reset(); //initialize board
+function checkBoard(); //if board is full
+function switchTurns();
+function checkWin(); //check for winning values
+function validPlay(); //check if position has been filled or not
